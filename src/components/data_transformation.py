782c3728fb158a3192fa66fc6dd716dfcd06ceea
Abderrahmane El Oufir <abder.eloufir@gmail.com>
2025-09-11 15:57:48 -0500

    Flask app update

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -42,53 +42,14 @@ class DataTransformation:
 
             if numerical_columns is None:
                 numerical_columns = [
-                    'time_in_hospital','num_lab_procedures','num_procedures','num_medications',
-                    'number_outpatient','number_emergency','number_inpatient','number_diagnoses',
-                    'MP_DM_payer_ind','mb_admission_grp_1_ct','mb_admission_grp_2_ct',
-                    'mb_discharge_grp_1_ct','mb_discharge_grp_2_ct','mb_admission_type_ct',
-                    'distinct_diag_count','diag_1_freq','diag_2_freq','diag_3_freq',
-                    'diag_1_428_ind','diag_1_491_ind','diag_1_493_ind','diag_2_403_ind',
-                    'diag_2_707_ind','diag_2_585_ind','diag_2_491_ind','diag_3_403_ind',
-                    'diag_3_585_ind','diag_3_707_ind','diag_1_driver_ind','diag_2_driver_ind',
-                    'diag_3_driver_ind','diagnosis_tuple_freq','diag_1_sort_freq','diag_2_sort_freq',
-                    'diag_3_sort_freq','high propensity tuple ind','dx_428_ind_max','dx_428_ind_sum',
-                    'dx_403_ind_max','dx_403_ind_sum','dx_707_ind_max','dx_707_ind_sum','dx_585_ind_max',
-                    'dx_585_ind_sum','dx_491_ind_max','dx_491_ind_sum','dx_396_ind_max','dx_396_ind_sum',
-                    'dx_440_ind_max','dx_440_ind_sum','dx_453_ind_max','dx_453_ind_sum','dx_571_ind_max',
-                    'dx_571_ind_sum','dx_284_ind_max','dx_284_ind_sum','dx_304_ind_max','dx_304_ind_sum',
-                    'dx_482_ind_max','dx_482_ind_sum','dx_150_ind_max','dx_150_ind_sum','dx_282_ind_max',
-                    'dx_282_ind_sum','dx_332_ind_max','dx_332_ind_sum','dx_443_ind_max','dx_443_ind_sum',
-                    'dx_719_ind_max','dx_719_ind_sum','dx_423_ind_max','dx_423_ind_sum','dx_281_ind_max',
-                    'dx_281_ind_sum','dx_536_ind_max','dx_536_ind_sum','dx_368_ind_max','dx_368_ind_sum',
-                    'dx_515_ind_max','dx_515_ind_sum','dx_595_ind_max','dx_595_ind_sum','dx_572_ind_max',
-                    'dx_572_ind_sum','dx_681_ind_max','dx_681_ind_sum','dx_581_ind_max','dx_581_ind_sum',
-                    'dx_537_ind_max','dx_537_ind_sum','dx_490_ind_max','dx_490_ind_sum','dx_583_ind_max',
-                    'dx_583_ind_sum','dx_V46_ind_max','dx_V46_ind_sum','dx_519_ind_max','dx_519_ind_sum',
-                    'dx_300_ind_max','dx_300_ind_sum','dx_567_ind_max','dx_567_ind_sum','dx_E92_ind_max',
-                    'dx_E92_ind_sum','dx_V49_ind_max','dx_V49_ind_sum','dx_094_ind_max','dx_094_ind_sum',
-                    'dx_514_ind_max','dx_514_ind_sum','dx_494_ind_max','dx_494_ind_sum','dx_042_ind_max',
-                    'dx_042_ind_sum','dx_404_ind_max','dx_404_ind_sum','dx_346_ind_max','dx_346_ind_sum',
-                    'dx_792_ind_max','dx_792_ind_sum','dx_398_ind_max','dx_398_ind_sum','dx_753_ind_max',
-                    'dx_753_ind_sum','dx_577_ind_max','dx_577_ind_sum','dx_730_ind_max','dx_730_ind_sum',
-                    'dx_444_ind_max','dx_444_ind_sum','dx_459_ind_max','dx_459_ind_sum','dx_790_ind_max',
-                    'dx_790_ind_sum','dx_337_ind_max','dx_337_ind_sum','dx_397_ind_max','dx_397_ind_sum',
-                    'dx_292_ind_max','dx_292_ind_sum','dx_V42_ind_max','dx_V42_ind_sum','dx_289_ind_max',
-                    'dx_289_ind_sum','alcohol_history_ind','obesity_history_ind','mh_history_ind',
-                    'encounter_ct','mb_time_in_hospital','mb_readmitted_lt30_ct','mb_readmitted_gt30_ct',
-                    'mb_readmitted_no_ct','mb_num_lab_procedures_ct','mb_num_procedures_ct',
-                    'mb_num_medications_ct','mb_number_outpatient_ct','mb_number_emergency_ct',
-                    'mb_number_inpatient_ct','mb_number_diagnoses_ct','mb A1c gt 7 ct','mb A1c gt 8 ct',
-                    'mb A1c Norm ct'
+                    'mb_readmitted_gt30_ct','mb_readmitted_lt30_ct','mb_readmitted_no_ct',
+                    'distinct_diag_count','encounter_ct','number_inpatient',
+                    'mb_number_inpatient_ct','mb_number_diagnoses_ct'
                 ]
 
             if categorical_columns is None:
                 categorical_columns = [
-                    'race','gender','age','weight','medical_specialty','diag_1','diag_2','diag_3',
-                    'max_glu_serum','A1Cresult','metformin','glimepiride','glipizide','glyburide',
-                    'pioglitazone','rosiglitazone','insulin','change','diabetesMed','admission_type',
-                    'discharge_disposition','admission_source','diagnosis_1','diagnosis_2','diagnosis_3',
-                    'diag_1_sort','diag_2_sort','diag_3_sort','diagnosis_1_sort','diagnosis_2_sort',
-                    'diagnosis_3_sort'
+                    'admission_type','A1Cresult'
                 ]
 
             num_pipeline = Pipeline(steps=[
